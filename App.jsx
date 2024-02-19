007c32d19373f70d8a85b2933445f223ed8a1af9
Ayush Kumar <97726129+ayush6041@users.noreply.github.com>
2024-02-20 00:00:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Component1 from "./Components/AppName";
+import './App.css';
+import AppName from "./Components/AppName";
+function App() {
+  return (
+    <div>
+      <AppName/>
+    </div>
+    
+  );
+}
+
+export default App;
